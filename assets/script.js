8b392347925e202a94f685024d6c79a09b8cfaf9
Elisa Urda <urda.elisa@gmail.com>
2018-03-25 14:38:52 -0400

    finished recipe ajax call and function)

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,11 @@ var firstPlaylist = "";
 var secondPlaylist = "";
 var thirdPlaylist = "";
 
+// GLOBAL VARIABLES FOR RECIPE ARRAY
+var firstRecipe = "";
+var secondRecipe = "";
+var thirdRecipe = "";
+
 $(document).ready(function () {
 
   // Initiallizes "Select" elements in forms and modal sequence
@@ -55,7 +60,7 @@ $(document).ready(function () {
   // EVENT LISTENERS FOR ALL FORM INPUTS
   $("#playlistOptions").on("change", function () {
     chosenPlaylist = parseInt($(this).find(":selected").val());
-    youtubeCall();
+    // youtubeCall();
   });
 
   $("#mainIngredient").on("change", function () {
@@ -73,18 +78,35 @@ $(document).ready(function () {
     console.log(diet);
   })
 
-  $("#allergies").on("change", function () {
-    allergies = $(this).find(":selected").val();
-    console.log(allergies);
-    allergyArr.push(allergies);
-    console.log(allergyArr);
-    // yummlyRecipeCall();
-  })
+  $(":checkbox").on("change", function(){
+    if (this.checked) {
+      allergies = $(this).attr("id");
+      console.log(allergies);
+      allergyArr.push(allergies);
+      console.log(allergyArr);
+    }
+    else {
+      allergies = $(this).attr("id");
+      console.log(allergies);
+      for (var i = allergyArr.length - 1; i>=0; i--){
+        if (allergyArr[i] === allergies) {
+          allergyArr.splice(i, 1);
+        }
+      }
+      console.log(allergyArr);
+    }
+  });
+
 
   $("#Food-Surprise").on("click", function () {
     event.preventDefault();
     console.log("Food Surprise Clicked");
   })
+
+  $("#modal-5-trigger").on("click", function (){
+    yummlyRecipeCall();
+  })
+
 
   $("#alcohol-yes").on("change", function () {
     alcohol = true
@@ -211,21 +233,100 @@ var dietArr= ["387^Lacto-ovo vegetarian", "386^Vegan", "403^Paleo", ""];
 
 var cors_anywhere_url = 'https://cors-anywhere.herokuapp.com/';
 var recipeURL = "http://api.yummly.com/v1/api/recipes?_app_id=9e74b819&_app_key=b87669ce79a8dc3323432bf6424282ab&q=" + mainIngredient + "&allowedCuisine[]=" + cuisineArr[cuisine] + "&allowedDiet[]=" + dietArr[diet];
+
+// IF AN ALLERGY IS SELECTED RUN THE FOLLOWING FOR LOOP TO INSERT ALL ALLERGIES INTO THE API CALL
 if (allergyArr.length>0) {
-  var withAllergyURL = "http://api.yummly.com/v1/api/recipes?_app_id=9e74b819&_app_key=b87669ce79a8dc3323432bf6424282ab&q=" + mainIngredient + "&allowedCuisine[]=" + cuisineArr[cuisine] + "&allowedDiet[]=" + dietArr[diet] + "&allowedAllergy[]=" + allergyArr[allergies];
+  var recipeURL = "http://api.yummly.com/v1/api/recipes?_app_id=9e74b819&_app_key=b87669ce79a8dc3323432bf6424282ab&q=" + mainIngredient + "&maxResult=25&allowedCuisine[]=" + cuisineArr[cuisine] + "&allowedDiet[]=" + dietArr[diet] + "&allowedAllergy[]=" + allergyArr[allergies];
   for (var i=0; i<allergyArr.length; i++){
-    // use .concat()
+    recipeURL.concat(allergyArr[i] = ",");
   }
-}
-var withAllergyURL = 
+};
+
 var corsRecipeURL = cors_anywhere_url + recipeURL;
 $.ajax({
   url: corsRecipeURL,
   method: "GET"
 }).then(function (response){
   console.log(response);
+  var recipeResults = response.matches
+
+      // MATH TO CHOOSE RANDOM NUMBERS FOR WHICH INDEX WE ARE GOING TO USE
+      var recipe1 = Math.floor(Math.random() * recipeResults.length);
+      console.log(recipe1);
+
+      var recipe2 = Math.floor(Math.random() * recipeResults.length);
+      if (recipe1 === recipe2) {
+        recipe2 = Math.floor(Math.random() * recipeResults.length);
+        console.log(recipe2);
+      }
   
-  var results = response.matches
+      var recipe3 = Math.floor(Math.random() * recipeResults.length);
+      if (recipe1 === recipe3 || recipe2 === recipe3) {
+        recipe3 = Math.floor(Math.random() * recipeResults.length);
+        console.log(recipe3);
+      }
+
+      var recipeArr = [
+        firstRecipe = {
+          name: recipeResults[recipe1].recipeName,
+          link: "https://www.yummly.com/#recipe/" + recipeResults[recipe1].id,
+          image: recipeResults[recipe1].smallImageUrls[0],
+          time: recipeResults[recipe1].totalTimeInSeconds
+        },
+        secondRecipe = {
+          name: recipeResults[recipe2].recipeName,
+          link: "https://www.yummly.com/#recipe/" + recipeResults[recipe2].id,
+          image: recipeResults[recipe2].smallImageUrls[0],
+          time: recipeResults[recipe2].totalTimeInSeconds
+        },
+        thirdRecipe = {
+          name: recipeResults[recipe3].recipeName,
+          link: "https://www.yummly.com/#recipe/" + recipeResults[recipe3].id,
+          image: recipeResults[recipe3].smallImageUrls[0],
+          time: recipeResults[recipe3].totalTimeInSeconds
+        }
+      ];
+      console.log(recipeArr);
+
+          //FOR LOOP TO PUSH THE INFORMATION INTO THE CORRECT AREAS OF THE HTML
+    for (var i = 0; i < recipeArr.length; i++) {
+      var recipeName = $("<h4>");
+      var recipeTime = $("<p>");
+      var recipeLink = $("<a>");
+      var recipeImage = $("<img>");
+
+      // LINKING AND CREATING THE NAME ELEMENT
+      recipeName.html(recipeArr[i].name);
+      recipeLink.attr("href", recipeArr[i].link);
+      recipeLink.attr("target", "blank");
+      recipeLink.html(recipeName);
+
+      // CREATING THE TIME ELEMENT
+      var timeInMinutes = moment.utc(recipeArr[i].time*1000).format("mm");
+      console.log(timeInMinutes);
+      recipeTime.text("Prep time: " + timeInMinutes + " minutes");
+
+      // CREATING THE IMAGE ELEMENT
+      recipeImage.attr("src", recipeArr[i].image);
+      recipeImage.addClass("responsive-img left");
+
+      // ENSURING THAT THE CORRECT RECIPE GOES INTO THE CORRECT PART OF THE CARD IN THE HTML
+      if (i === 0) {
+        $("#recipe1").append(recipeLink);
+        $("#recipe1").append(recipeTime);
+        $("#recipe1").prepend(recipeImage);
+      }
+      else if (i === 1) {
+        $("#recipe2").append(recipeLink);
+        $("#recipe2").append(recipeTime);
+        $("#recipe2").prepend(recipeImage);
+      }
+      else {
+        $("#recipe3").append(recipeLink);
+        $("#recipe3").append(recipeTime);
+        $("#recipe3").prepend(recipeImage);
+      }
+    }
 
 
 })
